f65048ca2063ee59e427a14d530d0b49a4706d77
Zhi-CMGT <0980617@hr.nl>
2026-06-09 15:45:37 +0200

    UploadPage.js bestand aangemaakt Navigatie in app.js bijgewerkt
    Installaties:
    -react native-stack
     -npx expo install @react-navigation/native-stack

--- packages/frontend2/App.js
+++ packages/frontend2/App.js
@@ -1,10 +1,26 @@
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import BottomTabNavigator from './components/BottomTabNavigator';
+import UploadPage from "./screens/UploadPage";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <BottomTabNavigator/>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="MainTabs"
+                    component={BottomTabNavigator}
+                    options={{headerShown: false}}
+                />
+
+                <Stack.Screen
+                    name="UploadPage"
+                    component={UploadPage}
+                    options={{title: 'Upload'}}
+                />
+
+            </Stack.Navigator>
         </NavigationContainer>
     );
 }

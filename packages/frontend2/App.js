fffce8bd939798d296b1637577e6a77642290f6a
Thomasplie <1039350@hr.nl>
2026-06-11 13:48:36 +0200

    Package.json Added stack

--- packages/frontend2/App.js
+++ packages/frontend2/App.js
@@ -1,10 +1,40 @@
-import { NavigationContainer } from "@react-navigation/native";
-import BottomTabNavigator from "./components/BottomTabNavigator";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import BottomTabNavigator from './components/BottomTabNavigator';
+import UploadPage from "./screens/UploadPage";
+import RegisterScreen from "./screens/register";
+import LocationPage from "./screens/LocationPage";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
-  return (
-    <NavigationContainer>
-      <BottomTabNavigator />
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="MainTabs"
+                    component={BottomTabNavigator}
+                    options={{headerShown: false}}
+                />
+
+                <Stack.Screen
+                    name="UploadPage"
+                    component={UploadPage}
+                    options={{title: 'Upload'}}
+                />
+
+                <Stack.Screen
+                    name="RegisterPage"
+                    component={RegisterScreen}
+                    options={{title: 'Register'}}
+                />
+
+                <Stack.Screen
+                    name="LocationPage"
+                    component={LocationPage}
+                    options={{title: 'Location'}}
+                />
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }

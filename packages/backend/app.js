364234f760ae5d4041f4bc579ae5d78e039f36a3
tooyaigarashi <igarashitooya5@gmail.com>
2026-06-17 16:19:38 +0200

    CORS changed

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: process.env.CORS_ORIGIN || "*",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Accept', 'Content-Type', 'x-api-key'],
+    allowedHeaders: ['Accept', 'Content-Type', 'x-api-key', 'Authorization'],
 }));
 
 app.use(validateApiKey);
